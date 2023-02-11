5bb82628245df95c53d24f47b4a3910970ecc77b
MccMiles <107641357+MccMiles@users.noreply.github.com>
2023-02-11 13:51:03 -0600

    final

--- backend/db/seeders/20230211183819-demo-user.js
+++ backend/db/seeders/20230211183819-demo-user.js
@@ -11,16 +11,22 @@ module.exports = {
     options.tableName = 'Users';
     return queryInterface.bulkInsert(options, [
       {
+        firstName: 'Spyro',
+        lastName: 'the Dragon',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Sonic',
+        lastName: 'the Hedgehog',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Crash',
+        lastName: 'Bandicoot',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
