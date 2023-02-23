6d9f14002571974d2a6e2e6d5f08c8888be0da7e
MccMiles <107641357+MccMiles@users.noreply.github.com>
2023-02-22 23:43:14 -0600

    fixed booking

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -1,33 +1,58 @@
 'use strict';
-const { Model } = require('sequelize');
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Booking extends Model {
-
+  class booking extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
-      Booking.belongsTo(models.User, {foreignKey: 'userId'});
-      Booking.belongsTo(models.Spot, {foreignKey: 'spotId'});
+      // define association here
     }
-  }
-  Booking.init({
-    spotId: DataTypes.INTEGER,
-    userId: DataTypes.INTEGER,
-    startDate: DataTypes.DATE,
-    endDate: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      validate: {
-        message: "Validation Error",
-
-        isAfterStartDate: function(currDate) {
-          if (currDate <= this.startDate) {
-            throw new Error('endDate cannot be on or before startDate');
+  }'use strict';
+  const { Model } = require('sequelize');
+  module.exports = (sequelize, DataTypes) => {
+    class Booking extends Model {
+  
+      static associate(models) {
+        Booking.belongsTo(models.User, {foreignKey: 'userId'});
+        Booking.belongsTo(models.Spot, {foreignKey: 'spotId'});
+      }
+    }
+    Booking.init({
+      spotId: DataTypes.INTEGER,
+      userId: DataTypes.INTEGER,
+      startDate: DataTypes.DATE,
+      endDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        validate: {
+          message: "Validation Error",
+  
+          isAfterStartDate: function(currDate) {
+            if (currDate <= this.startDate) {
+              throw new Error('endDate cannot be on or before startDate');
+            }
           }
         }
       }
-    }
+    }, {
+      sequelize,
+      modelName: 'Booking',
+    });
+    return Booking;
+  };
+  booking.init({
+    spotId: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER,
+    startDate: DataTypes.DATE,
+    endDate: DataTypes.DATE
   }, {
     sequelize,
-    modelName: 'Booking',
+    modelName: 'booking',
   });
-  return Booking;
+  return booking;
 };
